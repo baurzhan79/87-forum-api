98312abf0240f03bb90a7e85eef9fa2055fa817e
Baurzhan Yussupov <baurzhangulnur@gmail.com>
2023-07-22 18:16:34 +0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./config");
+
+const app = express();
+const port = 8000;
+
+const corsOption = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOption));
+app.use(express.json());
+app.use(express.static("public"));
+
+const runMongoose = async () => {
+    await mongoose.connect(config.db.url + "/" + config.db.name, { useNewUrlParser: true });
+    console.log("mongoose connected");
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+
+};
+
+runMongoose();
